3ad1531e06a30f0601ea60261ef884256c2321fc
unknown <vivekvelugubantla114@gmail.com>
2025-04-15 16:08:10 +0530

    Adding the test case scenario, which was already handelled by previous commit.

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -9,5 +9,9 @@ RSpec.describe StringCalculator do
     it 'returns the number for a single number' do
       expect(StringCalculator.add("1")).to eq(1)
     end
+
+    it 'returns the sum of two numbers' do
+      expect(StringCalculator.add("1,2")).to eq(3)
+    end
   end
 end
