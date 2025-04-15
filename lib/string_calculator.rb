25f3c4587d08a77e9e4076149d1cc181149fccb2
unknown <vivekvelugubantla114@gmail.com>
2025-04-15 16:37:25 +0530

    fixing the "supports different delimiters" scenario to work.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,9 +2,17 @@ class StringCalculator
     def self.add(input)
       return 0 if input.empty?
       
-      input.gsub("\n", ",")
-           .split(",")
-           .map(&:to_i)
-           .sum
+      delimiter = ','
+      numbers = input
+
+      if input.start_with?('//')
+        delimiter = input[2]
+        numbers = input.split("\n", 2)[1]
+      end
+
+      numbers.gsub("\n", delimiter)
+             .split(delimiter)
+             .map(&:to_i)
+             .sum
     end
 end
