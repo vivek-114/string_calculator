aa00a618c81a9c9bd27f6cfce3f9404a8758252b
unknown <vivekvelugubantla114@gmail.com>
2025-04-15 16:17:40 +0530

    Adding the specs until next failure scenario.

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -13,5 +13,13 @@ RSpec.describe StringCalculator do
     it 'returns the sum of two numbers' do
       expect(StringCalculator.add("1,2")).to eq(3)
     end
+
+    it 'returns the sum of multiple numbers' do
+      expect(StringCalculator.add("1,2,3,4,5")).to eq(15)
+    end
+
+    it 'handles new lines between numbers' do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
   end
 end
