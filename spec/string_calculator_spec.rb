e8f4e1077624554a3d2cec96c885847250a8b273
unknown <vivekvelugubantla114@gmail.com>
2025-04-15 16:03:48 +0530

    falling sceario for "returns the number for a single number"

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe StringCalculator do
     it 'returns 0 for an empty string' do
       expect(StringCalculator.add("")).to eq(0)
     end
+
+    it 'returns the number for a single number' do
+      expect(StringCalculator.add("1")).to eq(1)
+    end
   end
 end
